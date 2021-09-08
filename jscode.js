4609fa739ea80d58bda00c07c1a97e717f4f6b4a
Vasanthamalika <malika.sagar@gmail.com>
2021-09-08 21:45:42 +0530

    ExerciseLevel0

--- jscode.js
+++ jscode.js
@@ -314,3 +314,50 @@ function convertToF(celsius) {
   }
   
   convertToF(36.88);
+
+  //The switch statement is a flow-control statement that is similar to the if else statement. 
+  //switch statement is usednto control the complex conditional operations.
+  //switch statement is used to replace a statement that consists of complicated  if else statements chained together. 
+var month = 6;
+var monthName;
+switch (month) {
+    case 1:
+        monthName = 'January';
+        break;
+    case 2:
+        monthName = 'February';
+        break;
+    case 3:
+        monthName = 'March';
+        break;
+    case 4:
+        monthName = 'April';
+        break;
+    case 5:
+        monthName = 'May';
+        break;
+    case 6:
+        monthName = 'June';
+        break;
+    case 7:
+        monthName = 'July';
+        break;
+    case 8:
+        monthName  =   'August';
+        break;
+    case 9:
+        monthName   =   'September';
+        break;
+    case 10:
+        monthName   =   'October';
+        break;
+    case 11:
+        monthName   =   'November';
+        break;
+    case 12:
+        monthName   =   'December';
+    default:
+        monthName = 'Invalid month';
+}
+console.log(monthName); // June
+ 
